8a942374388fed856a5165c7074f9efd70d40dec
musaffarulislam <musaffarulislam@gmail.com>
2025-06-22 18:41:55 +0530

    feat: report page and bug fixes

--- src/components/reportPage/QuestionSection.tsx
+++ src/components/reportPage/QuestionSection.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const QuestionSection = () => {
   return (
     <div className='w-full my-8 flex  flex-col gap-2 justify-between items-center  text-center bg-primary p-8 rounded-xl py-16'>
-      <span className='text-lg'>Questions? We're here to help.</span>
+      <span className='text-lg'>Questions? We're here to help.</span>
       <span className='text-lg font-bold underline underline-offset-2'>Customer Support | FAQ & Glossary</span>
     </div>
   )
